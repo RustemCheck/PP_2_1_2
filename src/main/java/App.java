29e83efd4addf88ffa4719276d4bade607662fec
RustemCheck <rustemzinnatullin165@gmail.com>
2023-10-31 20:38:05 +0300

    First spring project. Creating a bean.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld beanTwo = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(beanTwo.getMessage());
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        Cat beanCatTwo = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getName());
+        System.out.println(beanCatTwo.getName());
+
+        System.out.println("Сравнение бинов Hello World: " + (bean == beanTwo));
+        System.out.println("Сравнение бинов Cat: " + (beanCat == beanCatTwo));
     }
 }
